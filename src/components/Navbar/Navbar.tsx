05efb54188c25cb9f84f4fa16c45ffdbb93417f5
Elisee Augustin <elisee@Elisees-MacBook-Pro.local>
2022-09-27 22:02:27 +0200

    added styling and positioning to nav, nav-items

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import NavbarItem from '../NavbarItem/NavbarItem';
+import './navbar.scss'
 
 const Navbar = () => {
     
+    return (
+        <section className='navbar'>
+            <div className='navbar__container'>
+                <div className='navbar__container__left'>
+                    <NavbarItem anchor='#' title='Home' icon={{imageUrl: 'https://cdn-icons-png.flaticon.com/512/263/263115.png', altText: 'home'}} />
+                    <NavbarItem anchor='#' title='Moments' icon={{imageUrl: 'https://cdn-icons-png.flaticon.com/512/248/248053.png', altText: 'moments'}} />
+                    <NavbarItem anchor='#' title='Notifications' icon={{imageUrl: 'https://cdn-icons-png.flaticon.com/512/2529/2529521.png', altText: 'bell'}} />
+                    <NavbarItem anchor='#' title='Messages' icon={{imageUrl: 'https://cdn-icons-png.flaticon.com/512/2089/2089181.png', altText: 'messages'}} />
+                </div>
+
+                <div className='navbar__container__center'>
+                    <NavbarItem anchor='#' icon={{imageUrl: 'https://cdn-icons-png.flaticon.com/512/733/733579.png', altText: 'twitter'}} />
+                </div>
+
+                <div className='navbar__container__right'>
+
+                    <input className='navbar__container__right__input' type='text' placeholder='Search Twitter' />
+
+                    <button className='navbar__container__right__button'>
+                        Tweet
+                    </button>
+
+                </div>
+
+            </div>
+        </section>
+    )
 }
+
+export default Navbar;
--- src/components/NavbarItem/NavbarItem.tsx
+++ src/components/NavbarItem/NavbarItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './navbarItem.scss';
 
 interface ImageBlockInterface {
     imageUrl: string;
@@ -15,17 +16,30 @@ const NavbarItem = ({
     anchor,
     icon,
     title
-}) => {
+}: NavbarItemInterface) => {
 
     return (
         <section className='navbar-item'>
             <div className='navbar-item__container'>
 
-                <div>
-                    <img src={icon?.imageUrl} alt={icon?.altText} />
-                </div>
+                <a href={anchor} className='navbar-item__container__link'>
+                    
+                    { icon?.imageUrl && (
+                        <div className='navbar-item__container__link__icon-container'>
+                            <img className='navbar-item__container__link__icon-container__icon' src={icon?.imageUrl} alt={icon?.altText} />
+                        </div>
+                    )}
+
+                    {title && (
+                        <div className='navbar-item__container__link__title'>
+                            {title}
+                        </div>
+                    )}
+                </a>
 
             </div>
         </section>
     )
 }
+
+export default NavbarItem;
