cf2c660ffdb0922468a1a197c9317ca7fdc9684d
Elisee Augustin <elisee@Elisees-MacBook-Pro.local>
2022-09-27 22:13:41 +0200

    started a profile section

---
+++ src/components/Profile-Section/ProfileSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProfileSection = () => {
+
+    return (
+        <section className='profile-section'>
+            <div className='profile-section__container'>
+
+                
+
+            </div>
+        </section>
+    )
+}
